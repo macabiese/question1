e605848b5a51d89ac83215b592c027aa7a55a36c
macabiese <macabiese@educ.college-montreal.qc.ca>
2022-10-12 08:26:50 -0400

    tp2

--- TP2.py
+++ TP2.py
@@ -4,41 +4,47 @@
 # importations
 from random import *
 
-# definition des variables
-nb_mystere = randint(0,1000)
-fin_jeu = False
-nb_essais = 0
-rejouer = None
+borne_min = int(input("Rentrez la borne minimale:_"))
+borne_max = int(input("Rentrez la borne maximale:_"))
 
+def jeu():
 
-#boucle qui prends fin lorsque le joueur ne veut plus jouer
-while fin_jeu == False:
-    nb_essais = nb_essais + 1
+    # definition des variables
+    fin_jeu = False
+    nb_mystere = randint(borne_min, borne_max)
+    rejouer = None
+    nb_essais = 0
 
-    # Introduction au jeu
-    if nb_essais <= 1:
-        print("J’ai choisi un nombre au hasard entre 1 et 1000. À vous de le deviner...")
+    #boucle qui prends fin lorsque le joueur ne veut plus jouer
+    while fin_jeu == False:
+        nb_essais = nb_essais + 1
 
-    # le joueur essaye de trouver le nombre mystere
-    nb_joueur = int(input("Entrez votre essai :_"))
+        # Introduction au jeu
+        if nb_essais <= 1:
+            print("J’ai choisi un nombre au hasard entre", borne_min, "et ", borne_max,". À vous de le deviner...")
 
-    # boucle qui dit au joueur si le nombre est plus grand, plus petit ou si il a gagné
-    if nb_joueur == nb_mystere:
-        print("Bravo! Bonne réponse.\nVous avez réussi en :", nb_essais,"essai(s).")
+        # le joueur essaye de trouver le nombre mystere
+        nb_joueur = int(input("Entrez votre essai :_"))
 
-        # demande au joueur si il veut rejouer et si ce nest pas le cas le code se termine
-        rejouer = input("Voulez-vous faire une autre partie (o/n)? _")
+        # boucle qui dit au joueur si le nombre est plus grand, plus petit ou si il a gagné
+        if nb_joueur == nb_mystere:
+            print("Bravo! Bonne réponse.\nVous avez réussi en :", nb_essais,"essai(s).")
 
-        if rejouer == "o":
-            nb_mystere = randint(0,1000)
-            nb_essais = 0
+            # demande au joueur si il veut rejouer et si ce nest pas le cas le code se termine
+            rejouer = input("Voulez-vous faire une autre partie (o/n)? _")
 
-        elif rejouer == "n":
-            print("Merci et au revoir...")
-            fin_jeu = True
+            if rejouer == "o":
+                nb_mystere = randint(0,1000)
+                nb_essais = 0
 
-    elif nb_joueur < nb_mystere:
-        print("Mauvais choix, le nombre est plus grand que", nb_joueur)
+            elif rejouer == "n":
+                print("Merci et au revoir...")
+                fin_jeu = True
 
-    elif nb_joueur > nb_mystere:
-        print("Mauvais choix, le nombre est plus petit que", nb_joueur)
+        elif nb_joueur < nb_mystere:
+            print("Mauvais choix, le nombre est plus grand que", nb_joueur)
+
+        elif nb_joueur > nb_mystere:
+            print("Mauvais choix, le nombre est plus petit que", nb_joueur)
+
+jeu()
